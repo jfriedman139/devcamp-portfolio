c3edb653abaa1b59d8ef01da69cb3420ee6a0125
Josh Friedman <jfriedman@Joshs-MBP.fios-router.home>
2018-03-18 18:05:15 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DevcampPortfolio::Application.config.secret_key_base = '19bc26876d98e09adbeebf21664fe71940617edb3e261864e02002739b5351928cfb91f76b6ea2cebc9ed3e09cda91769ff8c8810d1949f7f1fb082fc80b0028'
